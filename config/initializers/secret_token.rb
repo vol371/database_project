0a6b4b414e1612e02d618f1de6d65015c4524659
vol371 <vol371@yandex.ru>
2012-11-10 13:08:08 +0200

    empty_prjt

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DatabaseProject::Application.config.secret_token = '18cf45ca2e454f7648ebed0629bbd4abc6e8b4a9c05ea36f28c6ac644faf38b161f685180440e0d813b9f4356a7e9c214fe3a9d7a11e6d85fef916b1d82c5c59'
